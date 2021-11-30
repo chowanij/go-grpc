17ef75a7a5e4a06a1b8bf5dbecdddfe57f646d66
janchowaniec <janchowaniec@odrabiamy.pl>
2021-11-30 16:04:40 +0100

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "log"
+
+// Run - responsible for starting and initializing our grpc server
+func Run() error {
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chowanij/go-grpc
+
+go 1.17
